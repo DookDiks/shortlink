04987c249a416d5db4815f8b27d9386beed93fba
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-01-15 12:41:55 +0700

    feat: create get crons

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,6 +1,19 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export async function GET(request: Request) {
+  const expireLinks = await prisma.links.findMany({
+    where: {
+      expireAt: {
+        lte: new Date()
+      }
+    }
+  })
+
+  return NextResponse.json(expireLinks, { status: 200 });
+}
+
+
 const getExpireCrons = async () => {
   const history = await prisma.cronHistory.findMany({
     where: {
