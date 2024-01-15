c39d585374dffb50f1d0b387b549425a7df81347
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-01-15 12:39:13 +0700

    feat: create cron with update database

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,11 +1,24 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-  if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-    return NextResponse.json("Unauthorized", { status: 401 });
-  }
+const getExpireCrons = async () => {
+  const history = await prisma.cronHistory.findMany({
+    where: {
+      createdAt: {
+        lte: new Date(Date.now() - 24 * 60 * 60 * 1000)
+      }
+    }
+  })
 
+  const deleteHistory = history.map(h => prisma.cronHistory.delete({
+    where: { id: h.id }
+  }))
+
+  return deleteHistory
+
+}
+
+const getExpireLinks = async () => {
   const expireLinks = await prisma.links.findMany({
     where: {
       expireAt: {
@@ -18,8 +31,30 @@ export async function GET(request: Request) {
     where: { id: link.id }
   }))
 
+  return deleteLinks
+}
+
+export async function DELETE(request: Request) {
+  if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+
+  const expireLinks = await getExpireLinks()
+
+  const expireCrons = await getExpireCrons()
+
   try {
-    await Promise.all(deleteLinks)
+    const links = await Promise.all(expireLinks)
+
+    await Promise.all(expireCrons)
+
+    await prisma.cronHistory.create({
+      data: {
+        linkNumber: links.length
+      }
+    })
+
+
     return NextResponse.json({ ok: true });
   } catch (error) {
     return NextResponse.json({ ok: false, error }, { status: 400 });
