418b5032359b11db2667809bd7c61c3f46824315
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-01-15 13:02:24 +0700

    fix: change http method

--- src/app/api/cron/view/route.ts
+++ src/app/api/cron/view/route.ts
@@ -2,13 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const history = await prisma.cronHistory.findMany({
-    where: {
-      createdAt: {
-        lte: new Date(Date.now() - 24 * 60 * 60 * 1000)
-      }
-    }
-  })
+  const history = await prisma.cronHistory.findMany({})
 
   return NextResponse.json(history, { status: 200 });
 }
