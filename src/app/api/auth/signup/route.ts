853e39d07ff4e0939477ca5f7e7ed6990acb3f7c
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-01-20 14:24:26 +0700

    add: install new dependency

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import { prisma } from "@/lib/prisma";
 import { SignUpProps } from '@/components/auth/SignUpForm';
+import { tryCatch } from "@dookdiks/error"
 
 export async function POST(request: Request) {
   const { email, confirmPassword, password } = await request.json() as SignUpProps
@@ -9,24 +10,38 @@ export async function POST(request: Request) {
   if (!password) return Response.json({ target: "password", message: "Missing password" }, { status: 400 })
   if (!confirmPassword) return Response.json({ target: "confirmPassword", message: "Missing confirm password" }, { status: 400 })
 
-  try {
-    const existingUser = await prisma.user.findUnique({
-      where: {
-        email
-      }
-    })
 
-    if (existingUser) return Response.json({ target: "email", message: "User already exists" }, { status: 400 })
+  const existingUser = await tryCatch(() => prisma.user.findUnique({
+    where: {
+      email
+    }
+  }), (e) => Response.json({ error: e, message: "Something went wrong with prisma" }, { status: 500 }))
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password: await bcrypt.hash(password, 10),
-      }
-    })
+  // try {
+  //   const existingUser = await prisma.user.findUnique({
+  //     where: {
+  //       email
+  //     }
+  //   })
+  // } catch (error) {
+  //   return Response.json({ target: "email", message: "Something went wrong" }, { status: 500 })
+  // }
 
-    return Response.json(user, { status: 200 })
-  } catch (error) {
-    return Response.json({ error }, { status: 500 })
-  }
+  if (existingUser) return Response.json({ target: "email", message: "User already exists" }, { status: 400 })
+
+  const user = await tryCatch(async () => prisma.user.create({
+    data: {
+      email,
+      password: await bcrypt.hash(password, 10),
+    }
+  }), (e) => Response.json({ error: e, message: "Something went wrong with prisma" }, { status: 500 }))
+
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email,
+  //     password: await bcrypt.hash(password, 10),
+  //   }
+  // })
+
+  return Response.json(user, { status: 200 })
 }
