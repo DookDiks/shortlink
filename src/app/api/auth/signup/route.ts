88249c40ddeab9249ad0fe3679400b6795ef7462
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-01-20 12:18:28 +0700

    error: debuging

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -9,16 +9,17 @@ export async function POST(request: Request) {
   if (!password) return Response.json({ target: "password", message: "Missing password" }, { status: 400 })
   if (!confirmPassword) return Response.json({ target: "confirmPassword", message: "Missing confirm password" }, { status: 400 })
 
-  return Response.json("user", { status: 400 })
-
+  
   const existingUser = await prisma.user.findUnique({
     where: {
       email
     },
   })
-
+  
   if (existingUser) return Response.json({ target: "email", message: "User already exists" }, { status: 400 })
-
+  
+  return Response.json("user", { status: 400 })
+  
   const user = await prisma.user.create({
     data: {
       email,
