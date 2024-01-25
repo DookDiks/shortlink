0f1e27d987d61b3ed69187c485587ca9df1903ce
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-01-25 20:36:24 +0700

    fix font error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+import { Noto_Sans_Thai, Outfit, Sarabun } from "next/font/google";
 import "../styles/globals.scss";
+import { cn } from "@dookdiks/utils";
 
 export const metadata: Metadata = {
 	title: "DookDiks - Shorten links",
@@ -7,13 +9,39 @@ export const metadata: Metadata = {
 		"This web application is used to shorten links. It is built with Next.js and TailwindCSS.",
 };
 
+const notoSansThai = Noto_Sans_Thai({
+	subsets: ["latin", "thai"],
+	display: "auto",
+	variable: "--font-noto-sans-thai",
+});
+
+const outfit = Outfit({
+	subsets: ["latin", "latin-ext"],
+	display: "auto",
+	variable: "--font-outfit",
+});
+
+const sarabun = Sarabun({
+	weight: "400",
+	subsets: ["latin", "latin-ext", "thai"],
+	display: "auto",
+	variable: "--font-sarabun",
+});
+
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html
+			lang="en"
+			className={cn(
+				notoSansThai.className,
+				outfit.className,
+				sarabun.className
+			)}
+		>
 			<body>{children}</body>
 		</html>
 	);
